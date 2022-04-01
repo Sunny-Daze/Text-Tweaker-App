04b97c7c84e221a489b37f0316e7ee656e5689e8
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-04-01 20:55:43 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm"
+import TextForm from "./components/TextForm";
 
 function App() {
   const [mode, setmode] = useState("light");
@@ -10,30 +10,33 @@ function App() {
 
   const showalert = (message, type) => {
     setalert({
-      msg : message,
-      typ : type
-    })
+      msg: message,
+      typ: type,
+    });
     setTimeout(() => {
-      setalert(null)
+      setalert(null);
     }, 1500);
-  }
- const toggle = () => {
-   if(mode === "light"){
-     setmode("dark");
-     document.body.style.backgroundColor = "#042743";
-     showalert("Dark mode has been enabled!", "success")
-    }
-    else{
-      setmode("light")
+  };
+  const toggle = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showalert("Dark mode has been enabled!", "success");
+    } else {
+      setmode("light");
       document.body.style.backgroundColor = "white";
-      showalert("Light mode has been enabled!", "success")
-   }
- }
+      showalert("Light mode has been enabled!", "success");
+    }
+  };
   return (
     <>
       <Navbar title="String Sister" mode={mode} toggle={toggle} />
       <Alert alert={alert} />
-      <TextForm showalert={showalert} heading="Enter the text to analyze below" mode={mode} />
+      <TextForm
+        showalert={showalert}
+        heading="Enter the text to analyze below"
+        mode={mode}
+      />
     </>
   );
 }
